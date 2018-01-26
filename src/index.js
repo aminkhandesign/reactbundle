c119afceaf313ac44ca014b9050dfc041aa69038
codeclub <codeclub@LAUSD-MBP-2017-4.local>
2018-01-26 02:14:25 -0800

    setting up environment

---
+++ src/index.js
@@ -0,0 +1,11 @@
+class Mycomp extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>hello worldddd </h1>
+            </div>    
+        )
+    }
+}
+
+ReactDOM.render(<Mycomp />, document.getElementById('main'))
